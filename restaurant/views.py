c9fd37706e01be7038b62837593f8f26d1dab38d
Jasonelp <jasonpercyjordan@gmail.com>
2026-05-08 09:28:31 -0500

    Añadir ViewSets para Plato y Pedido con filtro de búsqueda

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import viewsets, filters
+from .models import Plato, Pedido
+from .serializers import PlatoSerializer, PedidoSerializer
 
-# Create your views here.
+
+class PlatoViewSet(viewsets.ModelViewSet):
+    queryset = Plato.objects.all()
+    serializer_class = PlatoSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['nombre', 'categoria']
+
+
+class PedidoViewSet(viewsets.ModelViewSet):
+    queryset = Pedido.objects.all()
+    serializer_class = PedidoSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['estado']
